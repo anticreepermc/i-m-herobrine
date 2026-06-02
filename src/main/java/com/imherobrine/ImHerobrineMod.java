196ccf97ba24f7b34d7a33536be9492171aedee2
anticreepermc <mansur.abdulloev20@gmail.com>
2026-06-02 18:00:43 +0500

    Update ImHerobrineMod.java

--- src/main/java/com/imherobrine/ImHerobrineMod.java
+++ src/main/java/com/imherobrine/ImHerobrineMod.java
@@ -5,12 +5,14 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ImHerobrineMod.MODID)
 public final class ImHerobrineMod {
     public static final String MODID = "imherobrine";
 
-    public ImHerobrineMod(IEventBus modBus) {
+    public ImHerobrineMod() {
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         modBus.addListener(this::onCommonSetup);
         MinecraftForge.EVENT_BUS.register(HerobrineGameEvents.class);
     }
