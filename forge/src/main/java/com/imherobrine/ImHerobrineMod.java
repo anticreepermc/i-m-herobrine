bf27f9ad3201cdf6d7db02aa642a6dfd97bc5190
anticreepermc <mansur.abdulloev20@gmail.com>
2026-08-22 15:07:47 +0500

    commit

--- forge/src/main/java/com/imherobrine/ImHerobrineMod.java
+++ forge/src/main/java/com/imherobrine/ImHerobrineMod.java
@@ -2,6 +2,8 @@ package com.imherobrine;
 
 import com.imherobrine.network.HerobrineNetworking;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -10,6 +12,8 @@ public final class ImHerobrineMod {
     public static final String MODID = "imherobrine";
 
     public ImHerobrineMod(FMLJavaModLoadingContext context) {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                IExtensionPoint.DisplayTest.IGNORE_SERVER_VERSION);
         HerobrineNetworking.register();
         MinecraftForge.EVENT_BUS.register(HerobrineGameEvents.class);
     }
--- neoforge/src/main/java/com/imherobrine/neoforge/ImHerobrineNeoForge.java
+++ neoforge/src/main/java/com/imherobrine/neoforge/ImHerobrineNeoForge.java
@@ -2,6 +2,8 @@ package com.imherobrine.neoforge;
 
 import com.imherobrine.common.HerobrineGameLogic;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.IExtensionPoint;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartedEvent;
 import net.neoforged.fml.common.Mod;
@@ -9,6 +11,8 @@ import net.neoforged.fml.common.Mod;
 @Mod("imherobrine")
 public final class ImHerobrineNeoForge {
     public ImHerobrineNeoForge(IEventBus modBus) {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                IExtensionPoint.DisplayTest.IGNORE_SERVER_VERSION);
         modBus.addListener(NeoForgeNetworking::register);
         NeoForge.EVENT_BUS.addListener((ServerStartedEvent event) ->
                 HerobrineGameLogic.placeTotemIfNeeded(event.getServer().overworld()));
